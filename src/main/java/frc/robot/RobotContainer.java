57d1273dae9d88268e50b2a796728d5e0108ac4d
6817frc <frc6817@gmail.com>
2025-03-13 17:43:59 -0500

    added camera apriltag targeting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -183,6 +183,8 @@ public class RobotContainer {
 
 		double txnc = LimelightHelpers.getTXNC("");  // Horizontal offset from principal pixel/point to target in degrees
 		double tync = LimelightHelpers.getTYNC("");  // Vertical  offset from principal pixel/point to target in degrees
+
+	
 	}	
 
 	/**
@@ -194,10 +196,64 @@ public class RobotContainer {
 	 * passing it to a
 	 * {@link JoystickButton}.
 	 */
+private boolean cameraTargetingMode;
+
+double kMaxSpeed = 3.0;
+
+ // simple proportional turning control with Limelight.
+  // "proportional control" is a control algorithm in which the output is proportional to the error.
+  // in this case, we are going to return an angular velocity that is proportional to the 
+  // "tx" value from the Limelight.
+  double limelight_aim_proportional()
+  {    
+    // kP (constant of proportionality)
+    // this is a hand-tuned number that determines the aggressiveness of our proportional control loop
+    // if it is too high, the robot will oscillate around.
+    // if it is too low, the robot will never reach its target
+    // if the robot never turns in the correct direction, kP should be inverted.
+    double kP = .035;
+
+    // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the rightmost edge of 
+    // your limelight 3 feed, tx should return roughly 31 degrees.
+    double targetingAngularVelocity = LimelightHelpers.getTX("limelight") * kP;
+
+    // convert to speed for our drive method
+    targetingAngularVelocity *= kMaxSpeed;
+
+    //invert since tx is positive when the target is to the right of the crosshair
+    targetingAngularVelocity *= -1.0;
+
+    return targetingAngularVelocity;
+  }
+
+  // simple proportional ranging control with Limelight's "ty" value
+  // this works best if your Limelight's mount height and target mount height are different.
+  // if your limelight and target are mounted at the same or similar heights, use "ta" (area) for target ranging rather than "ty"
+  double limelight_range_proportional()
+  {    
+    double kP = .1;
+    double targetingForwardSpeed = LimelightHelpers.getTY("limelight") * kP;
+    targetingForwardSpeed *= kMaxSpeed;
+    targetingForwardSpeed *= -1.0;
+    return targetingForwardSpeed;
+  }
 
 	public void updateJoystick(double xSpeed, double ySpeed, double rot, Boolean fieldRelative, 
 	Boolean rateLimit, double leftYValue, double RightYValue, double leftTrig, double rightTrig) {
-		drivetrain.drive(xSpeed, ySpeed, rot, fieldRelative, rateLimit);
+
+// while the B-button is pressed, overwrite some of the driving values with the output of our limelight methods
+		if (cameraTargetingMode = true) {
+			final var side_limelight = limelight_aim_proportional();
+      		ySpeed = side_limelight;
+
+        	final var forward_limelight = limelight_range_proportional();
+        	xSpeed = forward_limelight;
+
+        	//while using Limelight, turn off field-relative driving.
+        	fieldRelative = false;
+		} else {
+			drivetrain.drive(xSpeed, ySpeed, rot, fieldRelative, rateLimit);
+		}
 		intake.armMove(leftYValue);
 		intake.wristMove(RightYValue);
 		intake.wheelOut(leftTrig);
@@ -239,9 +295,21 @@ public class RobotContainer {
 		}
 	}
 
+	public void buttonPressed() {
+		cameraTargetingMode = true;
+	}
+
+	public void buttonNotPressed() {
+		cameraTargetingMode = false;
+	}
+
 	private void configureButtonBindings() {
 
 		/*------------------ JoyMain ------------------*/
+
+		joyMain.button(2).onTrue(Commands.runOnce(() -> buttonPressed()));
+
+		joyMain.button(2).onFalse(Commands.runOnce(() -> buttonNotPressed()));
 
 		joyMain.start().onTrue(Commands.runOnce(() -> toggleRelative()));
 
