ec56ba60a3b7e3f146a2649f061a8527ddf01b5d
6817frc <frc6817@gmail.com>
2025-03-14 09:39:35 -0500

    fixed climb

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -25,6 +25,8 @@ public class Climber extends SubsystemBase {
   private double frontEncoderOffset;
   private double backEncoderOffset;
   private double realMotorPos;
+  private boolean leftManualMode;
+  private boolean RightManualMode;
 
   /** Creates a new Climber. */
   public Climber() {
@@ -45,10 +47,24 @@ public class Climber extends SubsystemBase {
 
   public void climbMove(double coLeftTrig) {
     m_climb.set(coLeftTrig); //set speed:0.35
+    if (coLeftTrig != 0) {
+      RightManualMode = true;
+      m_climb.set(coLeftTrig);
+    } else if (RightManualMode == true && coLeftTrig ==0) {
+      m_climb.set(0);
+      RightManualMode = false;
+    }
   }
 
   public void climbMoveRev(double coRightTrig) {
-    m_climb.set(coRightTrig); //set speed:-0.35
+    //set speed:-0.35
+    if (coRightTrig != 0) {
+      RightManualMode = true;
+      m_climb.set(coRightTrig);
+    } else if (RightManualMode == true && coRightTrig ==0) {
+      m_climb.set(0);
+      RightManualMode = false;
+    }
   }
 
   public void stopClimb() {
